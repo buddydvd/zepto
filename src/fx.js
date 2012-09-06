7d58f3b235af241e7181b9c27dcfda8598e20b41
Ali Mihandoost <i@ali.md>
2012-09-06 15:38:11 +0430

    support timing-function for animate()
    Closes #577

--- src/fx.js
+++ src/fx.js
@@ -42,6 +42,7 @@
       // keyframe animation
       cssProperties[prefix + 'animation-name'] = properties
       cssProperties[prefix + 'animation-duration'] = duration + 's'
+      cssProperties[prefix + 'animation-timing-function'] = (ease || 'linear')
       endEvent = $.fx.animationEnd
     } else {
       // CSS transitions
