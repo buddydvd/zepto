f9d3ca89a8efdc6ef544d173373007f387fcbd64
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-09-07 18:16:17 +0200

    enable unsetting css() rules by passing falsey values
    Any falsey value other than 0 will unset a rule:
     - '' (empty string)
     - undefined
     - null
     - false
    
    References #575

--- src/zepto.js
+++ src/zepto.js
@@ -481,7 +481,7 @@ var Zepto = (function() {
       }
     },
     css: function(property, value){
-      if (value === undefined && typeof property == 'string')
+      if (arguments.length < 2 && typeof property == 'string')
         return (
           this.length == 0
             ? undefined
@@ -489,13 +489,13 @@ var Zepto = (function() {
 
       var css = ''
       for (key in property)
-        if(typeof property[key] == 'string' && property[key] == '')
+        if (!property[key] && property[key] !== 0)
           this.each(function(){ this.style.removeProperty(dasherize(key)) })
         else
           css += dasherize(key) + ':' + maybeAddPx(key, property[key]) + ';'
 
       if (typeof property == 'string')
-        if (value === '')
+        if (!value && value !== 0)
           this.each(function(){ this.style.removeProperty(dasherize(property)) })
         else
           css = dasherize(property) + ":" + maybeAddPx(property, value)
